8fcd1aac1c80f20f753588cf6c9fb68cac595a20
kellyeryan <kellyeryan@gmail.com>
2020-05-15 13:38:55 -0400

    Set up classroom index page

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,2 +1,22 @@
 class ClassroomsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @classrooms = Classroom.all
+  end
+  def show
+    if authorized?
+      @classroom=Classroom.find(params[:id])
+      render :show
+    end
+  end
+  def create
+  end
+  def update
+  end
+  def destroy
+  end
+private
+  def authorized?
+    @classroom.user == current_user
+  end
 end
