1816b6edd82ff75ef4add6ecfd3a0547fcb275cd
kellyeryan <kellyeryan@gmail.com>
2020-05-15 13:19:53 -0400

    Fix up routes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,12 @@
 class StudentsController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def create
+  end
+  def update
+  end
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
     root "pages#my_classrooms", as: :authenticated_root
   end
   root 'pages#home'
+  resources :classrooms, only: [:index, :show, :create, :update, :destroy] do
+    resources :students, only: [:index, :show, :create, :update, :destroy]
+  end
 end
